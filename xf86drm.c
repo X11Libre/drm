bloomberg/chromium.bb

commit 293b95e81531dbfc1327cbaae125cc13e7fb074d

xf86drm: open correct render node on non-linux
drm render nodes have the same major as drm primary devices but offset
the minor by a base of 128.

I expected the name of the device to have numbering starting at 0 when
these non-linux codepaths were added (before OpenBSD had render nodes).

Signed-off-by: Jonathan Gray <jsg@jsg.id.au>
Acked-by: Eric Engestrom <eric.engestrom@intel.com>

xf86drm.c
@@ -2904,7 +2904,7 @@ static char *drmGetMinorNameForFD(int fd, int type)
     char buf[PATH_MAX + 1];
     const char *dev_name;
     unsigned int maj, min;
-    int n, base;
+    int n;
 
     if (fstat(fd, &sbuf))
         return NULL;
@@ -2929,11 +2929,7 @@ static char *drmGetMinorNameForFD(int fd, int type)
         return NULL;
     };
 
-    base = drmGetMinorBase(type);
-    if (base < 0)
-        return NULL;
-
-    n = snprintf(buf, sizeof(buf), dev_name, DRM_DIR_NAME, min - base);
+    n = snprintf(buf, sizeof(buf), dev_name, DRM_DIR_NAME, min);
     if (n == -1 || n >= sizeof(buf))
         return NULL;
 
@@ -3827,7 +3823,7 @@ drm_public int drmGetDevice2(int fd, uint32_t flags, drmDevicePtr *device)
     char             node[PATH_MAX + 1];
     const char      *dev_name;
     int              node_type, subsystem_type;
-    int              maj, min, n, ret, base;
+    int              maj, min, n, ret;
 
     if (fd == -1 || device == NULL)
         return -EINVAL;
@@ -3859,11 +3855,7 @@ drm_public int drmGetDevice2(int fd, uint32_t flags, drmDevicePtr *device)
         return -EINVAL;
     };
 
-    base = drmGetMinorBase(node_type);
-    if (base < 0)
-        return -EINVAL;
-
-    n = snprintf(node, PATH_MAX, dev_name, DRM_DIR_NAME, min - base);
+    n = snprintf(node, PATH_MAX, dev_name, DRM_DIR_NAME, min);
     if (n == -1 || n >= PATH_MAX)
       return -errno;
     if (stat(node, &sbuf))
@@ -4083,7 +4075,7 @@ drm_public char *drmGetDeviceNameFromFd2(int fd)
     char             node[PATH_MAX + 1];
     const char      *dev_name;
     int              node_type;
-    int              maj, min, n, base;
+    int              maj, min, n;
 
     if (fstat(fd, &sbuf))
         return NULL;
@@ -4112,11 +4104,7 @@ drm_public char *drmGetDeviceNameFromFd2(int fd)
         return NULL;
     };
 
-    base = drmGetMinorBase(node_type);
-    if (base < 0)
-        return NULL;
-
-    n = snprintf(node, PATH_MAX, dev_name, DRM_DIR_NAME, min - base);
+    n = snprintf(node, PATH_MAX, dev_name, DRM_DIR_NAME, min);
     if (n == -1 || n >= PATH_MAX)
       return NULL;
 

X11Libre/drm

commit f50dc823e73679746405dcc0c933e2774635da97

xf86drm: open real correct render node on non-linux
Tested only on OpenBSD.  Before:

	$ cat drm-name.c
	#include <fcntl.h>
	#include <stdio.h>
	#include <xf86drm.h>

	int
	main(void)
	{
	        int fd = open("/dev/dri/renderD128", O_RDONLY);
	        puts(drmGetRenderDeviceNameFromFd(fd));
	        puts(drmGetPrimaryDeviceNameFromFd(fd));
	        fd = open("/dev/dri/card0", O_RDONLY);
	        puts(drmGetPrimaryDeviceNameFromFd(fd));
	        puts(drmGetRenderDeviceNameFromFd(fd));
	}
	$ cc drm-name.c `pkg-config --cflags --libs libdrm`
	$ ./a.out
	/dev/dri/renderD256
	/dev/dri/card128
	/dev/dri/card0
	/dev/dri/renderD128

after:

	$ ./a.out
	/dev/dri/renderD128
	/dev/dri/card0
	/dev/dri/card0
	/dev/dri/renderD128

Fixes: 293b95e81531 ("xf86drm: open correct render node on non-linux")

xf86drm.c
@@ -3498,6 +3498,8 @@ static char *drmGetMinorNameForFD(int fd, int type)
     const char *dev_name = drmGetDeviceName(type);
     unsigned int maj, min;
     int n;
+    int base = drmGetMinorBase(type);
+    int renderbase = drmGetMinorBase(DRM_NODE_RENDER);
 
     if (fstat(fd, &sbuf))
         return NULL;
@@ -3511,7 +3513,10 @@ static char *drmGetMinorNameForFD(int fd, int type)
     if (!dev_name)
         return NULL;
 
-    n = snprintf(buf, sizeof(buf), dev_name, DRM_DIR_NAME, min);
+    if (min >= renderbase)
+        min -= renderbase;
+
+    n = snprintf(buf, sizeof(buf), dev_name, DRM_DIR_NAME, base + min);
     if (n == -1 || n >= sizeof(buf))
         return NULL;
 